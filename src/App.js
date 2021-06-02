fc9725f74a2d6a1ad16b8a843fc3a81e7464ff28
koki-noguchi <67818050+koki-noguchi@users.noreply.github.com>
2021-06-03 08:04:31 +0900

    useCallbackで再レンダリングを最適化 useCallback 関数をpropsで渡すと、関数が実行される際には新しく関数が生成されるため、再レンダリングが起きる。 useCallbackを使えば、第２引数で指定した配列が更新されない限り、関数の再生成が発生しない。

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { ChildArea } from "./components/ChildArea";
 import "./styles.css";
 
@@ -7,13 +7,14 @@ export default function App() {
   const [open, setOpen] = useState(false);
   const onChangeText = (e) => setText(e.target.value);
   const onClickOpen = () => setOpen(!open);
+  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
   return (
     <div className="App">
       <input value={text} onChange={onChangeText} />
       <br />
       <br />
       <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} />
+      <ChildArea open={open} onClickClose={onClickClose} />
     </div>
   );
 }
