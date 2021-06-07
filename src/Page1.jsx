1ff76f8362939abaffda6e2ceb710eebd47d3cfd
koki-noguchi <67818050+koki-noguchi@users.noreply.github.com>
2021-06-07 20:58:23 +0900

    Linkを使わずにページ遷移する方法を試した useHistoryを使い、push()の中身に遷移先urlを入れると実現される。 １つ前の画面に戻るには、pushの代わりにgoBackを使う

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,14 +1,19 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link, useHistory, useLocation } from "react-router-dom";
 
 export const Page1 = () => {
   const { state } = useLocation();
   console.log(state);
+  const history = useHistory();
+  const onClickPage1A = () => history.push("/page1/page1a");
+
   return (
     <div>
       <h1>Page1です</h1>
       <Link to="/page1/page1a">page1A</Link>
       <br></br>
       <Link to="/page1/page1b">page1B</Link>
+      <br />
+      <button onClick={onClickPage1A}>page1Aへ行く</button>
     </div>
   );
 };
--- src/Page1A.jsx
+++ src/Page1A.jsx
@@ -1,7 +1,12 @@
+import { useHistory } from "react-router-dom";
+
 export const Page1A = () => {
+  const history = useHistory();
+  const clickBackButton = () => history.goBack();
   return (
     <div>
       <p>Page1Aです</p>
+      <button onClick={clickBackButton}>戻る</button>
     </div>
   );
 };
