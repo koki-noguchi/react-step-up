a9df7e44a3cbc504211867f066aedeb7e9fd69d9
koki-noguchi <67818050+koki-noguchi@users.noreply.github.com>
2021-06-09 08:30:53 +0900

    2つ目のボタンを作成・共通するスタイルを別コンポーネントに切り出した

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import { PrimarryButton } from "./components/atoms/button/PrimarryButton";
+import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 
 export default function App() {
-  return <PrimarryButton>test</PrimarryButton>;
+  return (
+    <>
+      <PrimarryButton>test</PrimarryButton>
+      <SecondaryButton>test2</SecondaryButton>
+    </>
+  );
 }
