ffacbd28226060845f59fd8b20367541149f0496
koki-noguchi <67818050+koki-noguchi@users.noreply.github.com>
2021-06-11 08:16:36 +0900

    organismsを試した

---
+++ src/components/atoms/organisms/user/UserCard.jsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+import { Card } from "../../card/Card";
+import { UserNameWithImage } from "../../molecules/UserNameWithImage";
+
+export const UserCard = (props) => {
+  const { user } = props;
+  return (
+    <Card>
+      <UserNameWithImage image={user.image} name={user.name} />
+      <SDl>
+        <dt>メールアドレス</dt>
+        <dd>{user.email}</dd>
+        <dt>TEL</dt>
+        <dd>{user.phone}</dd>
+        <dt>会社名</dt>
+        <dd>{user.company.name}</dd>
+        <dt>WEB</dt>
+        <dd>{user.website}</dd>
+      </SDl>
+    </Card>
+  );
+};
+
+const SDl = styled.dl`
+  text-align: left;
+  margin-bottom: 0px;
+  dt {
+    float: left;
+  }
+  dd {
+    padding-left: 32px;
+    padding-bottom: 8px;
+    overflow-wrap: break-word;
+  }
+`;
